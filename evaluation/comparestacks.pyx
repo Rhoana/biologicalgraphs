572124feab8e90eff6a9bc4b058742380b1d5829
bmatejek <bmatejek@gmail.com>
2019-07-11 15:43:13 -0400

    evaluation: no longer use old adapted rand

--- evaluation/comparestacks.pyx
+++ evaluation/comparestacks.pyx
@@ -2,7 +2,6 @@ cimport cython
 cimport numpy as np
 import numpy as np
 import ctypes
-import scipy.sparse as sparse
     
 
 
@@ -13,79 +12,6 @@ from biologicalgraphs.transforms import distance, seg2seg
 
 cdef extern from 'cpp-comparestacks.h':
     double *CppEvaluate(long *segmentation, long *gold, long grid_size[3], long *ground_truth_masks, long nmasks)
-
-
-
-def adapted_rand(prefix, seg, gt, all_stats=False, dilate_ground_truth=2, filtersize=0):
-    """Compute Adapted Rand error as defined by the SNEMI3D contest [1]
-    Formula is given as 1 - the maximal F-score of the Rand index 
-    (excluding the zero component of the original labels). Adapted 
-    from the SNEMI3D MATLAB script, hence the strange style.
-    Parameters
-    ----------
-    seg : np.ndarray
-        the segmentation to score, where each value is the label at that point
-    gt : np.ndarray, same shape as seg
-        the groundtruth to score against, where each value is a label
-    all_stats : boolean, optional
-        whether to also return precision and recall as a 3-tuple with rand_error
-    Returns
-    -------
-    are : float
-        The adapted Rand error; equal to $1 - \frac{2pr}{p + r}$,
-        where $p$ and $r$ are the precision and recall described below.
-    prec : float, optional
-        The adapted Rand precision. (Only returned when `all_stats` is ``True``.)
-    rec : float, optional
-        The adapted Rand recall.  (Only returned when `all_stats` is ``True``.)
-    References
-    ----------
-    [1]: http://brainiac2.mit.edu/SNEMI3D/evaluation
-    """
-
-    # remove all small connected components
-    if filtersize > 0:
-        seg2seg.RemoveSmallConnectedComponents(seg, filtersize)
-        gt = seg2seg.RemoveSmallConnectedComponents(gt, filtersize)
-
-    if dilate_ground_truth > 0:
-        distance.DilateGoldData(prefix, gt, dilate_ground_truth)
-
-
-    # segA is truth, segB is query
-    segA = np.ravel(gt)
-    segB = np.ravel(seg)
-    n = segA.size
-
-    n_labels_A = np.amax(segA) + 1
-    n_labels_B = np.amax(segB) + 1
-
-    ones_data = np.ones(n)
-
-    p_ij = sparse.csr_matrix((ones_data, (segA[:], segB[:])), shape=(n_labels_A, n_labels_B))
-
-    a = p_ij[1:n_labels_A,:]
-    b = p_ij[1:n_labels_A,1:n_labels_B]
-    c = p_ij[1:n_labels_A,0].todense()
-    d = b.multiply(b)
-
-    a_i = np.array(a.sum(1))
-    b_i = np.array(b.sum(0))
-
-    sumA = np.sum(a_i * a_i)
-    sumB = np.sum(b_i * b_i) + (np.sum(c) / n)
-    sumAB = np.sum(d) + (np.sum(c) / n)
-
-    precision = sumAB / sumB
-    recall = sumAB / sumA
-
-    fScore = 2.0 * precision * recall / (precision + recall)
-    are = 1.0 - fScore
-
-    if all_stats:
-       return (are, precision, recall)
-    else:
-       return are
 
        
 
