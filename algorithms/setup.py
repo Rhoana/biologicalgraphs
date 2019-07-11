a7571650f474c83c90528b4930a901ec18f56a63
bmatejek <bmatejek@gmail.com>
2019-07-11 15:48:14 -0400

    algorithms: updated software directory for graph

--- algorithms/setup.py
+++ algorithms/setup.py
@@ -6,19 +6,19 @@ import os
 os.environ['CC'] = 'gcc'
 os.environ['CXX'] = 'g++'
 
-home_dir = os.path.expanduser('~')
+graph_software_dir = '{}/software/'.format(os.path.expanduser('~'))
 
 extensions = [
     Extension(
         name='multicut',
-        include_dirs=[np.get_include(), '{}/software/graph/include'.format(home_dir)],
+        include_dirs=[np.get_include(), '{}/graph/include'.format(graph_software_dir)],
         sources=['multicut.pyx', 'cpp-multicut.cpp'],
         extra_compile_args=['-O4', '-std=c++11'],
         language='c++'
     ),
     Extension(
         name='lifted_multicut',
-        include_dirs=[np.get_include(), '{}/software/graph/include'.format(home_dir)],
+        include_dirs=[np.get_include(), '{}/graph/include'.format(graph_software_dir)],
         sources=['lifted_multicut.pyx', 'cpp-lifted-multicut.cpp'],
         extra_compile_args=['-O4', '-std=c++11'],
         language='c++'
